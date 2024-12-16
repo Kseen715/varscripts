68b4eb66b28604bae0fbc0a749af430aab0cba91
kseen <korenevdn@gmail.com>
2024-12-16 18:16:06 +0300

    Add total size and compression ratio output to xz.py

--- xz.py
+++ xz.py
@@ -10,24 +10,29 @@ import subprocess
 import argparse
 import time
 
+total_size_before = 0
+total_size_after = 0
 
 def compress_file(file_name, threads):
+    global total_size_before, total_size_after
     cmd = ['xz', '-T{}'.format(threads), file_name]
     print(' '.join(cmd[:len(cmd) - 1] + ['"{}"'.format(file_name)]))
     file_size_before = os.path.getsize(file_name)
+    total_size_before += file_size_before
 
     start = time.time()
     subprocess.call(cmd)
     end = time.time()
 
     file_size_after = os.path.getsize(file_name + '.xz')
+    total_size_after += file_size_after
     ratio = 100 - (file_size_after / file_size_before * 100)
     print('\t> Took {:.6f}s'.format(end - start))
     print('\t> Compression ratio: {:.3f}%'.format(ratio))
 
 
 def decompress_file(file_name, threads):
-    cmd = ['xz', '-T0', '-d', file_name]
+    cmd = ['xz', '-T{}'.format(threads), '-d', file_name]
     print(' '.join(cmd[:len(cmd) - 1] + ['"{}"'.format(file_name)]))
 
     start = time.time()
@@ -71,3 +76,18 @@ if __name__ == '__main__':
                         compress_file(f, threads)
                 else:
                     compress_file(f, threads)
+        files = [f for f in files if not f.endswith('.xz')]
+        if len(files) > 1:
+            def get_size_str(size):
+                if size < 1024:
+                    return '{:.3f}B'.format(size)
+                elif size < 1024 * 1024:
+                    return '{:.3f}KB'.format(size / 1024)
+                elif size < 1024 * 1024 * 1024:
+                    return '{:.3f}MB'.format(size / 1024 / 1024)
+                else:
+                    return '{:.3f}GB'.format(size / 1024 / 1024 / 1024)
+            print('Total size before: {}'.format(get_size_str(total_size_before)))
+            print('Total size after: {}'.format(get_size_str(total_size_after)))
+            print('Total compression ratio: {:.3f}%'.format(100 - (total_size_after / total_size_before * 100)))
+
